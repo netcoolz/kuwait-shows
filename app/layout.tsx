981f08069cd4bd8d73881c3e6c6ba31c2218c490
ahmad alsaleh <netcoolz@me.com>
2026-04-03 19:47:52 +0300

    fix login popup + my horses auth + UI improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export const metadata = {
 
 
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.className} ${cairo.className} bg-black text-white`}>
